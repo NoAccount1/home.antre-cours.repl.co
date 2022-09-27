d291a82744ffcecf318f33462b811b1646a4fb96
NoAccount1 <mgrunnagel.spc@gmail.com>
2022-09-27 19:16:31 +0200

    Working on the vanilla webpage

---
+++ scripts/header.js
@@ -0,0 +1,3 @@
+// Used to load the same navbar on every page
+
+document.getElementsByTagName('header')[0].innerHTML ='<nav><a button href="./index.html"><i class="fa-solid fa-house"></i> Accueil</a><span separator></span><a button href="./planning/index.html">Planning</a></nav><div id="banner"></div>'
